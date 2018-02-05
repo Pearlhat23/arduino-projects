3c8fff47deb8537ff313fd679ba5a9b0c467022b
Jeremiah Smith <TheButtonBreaker@outlook.com>
2018-02-05 13:22:08 -0800

    Add files via upload

---
+++ mechanicalsense/mechanicalsense.ino
@@ -0,0 +1,25 @@
+int echoPin = 10;
+int vibratorPin = 13;
+int triggerPin = 9;
+
+int maxDistance = 10000;
+
+void setup(){
+  pinMode(echoPin, INPUT);
+  pinMode(triggerPin, OUTPUT);
+  pinMode(vibratorPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  digitalWrite(triggerPin, HIGH);
+  digitalWrite(triggerPin, LOW);
+  int pulseWidth = pulseIn(echoPin, HIGH, maxDistance);
+  Serial.println(pulseWidth);
+  if((pulseWidth < 100) || pulseWidth >= maxDistance){
+    pulseWidth = maxDistance;
+  }
+  pulseWidth = map(pulseWidth, 1000, 10000, 1000, 0);
+  analogWrite(vibratorPin, pulseWidth);
+}
+
